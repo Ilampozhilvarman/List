35ff60f1a6dbaac053b2ae0f7968610280480109
Ilampozhilvarman <Ilampozhilvarman@gmail.com>
2026-07-08 16:39:13 -0500

    Basic functions set up using pass keyword for later coding.

--- admin.py
+++ admin.py
@@ -1,2 +1,36 @@
 import json
 command = input("> ")
+args = command.split()
+
+def get_data_file():
+    pass
+
+def new_list(name):
+    pass
+
+def push_data(list_name, data):
+    pass
+
+def get_all_data_in_ls(list_name):
+    pass
+
+def get_all_data(list_name):
+    pass
+
+def get_data(list_name, index):
+    pass
+
+def delete_data(list_name, index):
+    pass
+
+def update_data(list_name, index, data):
+    pass
+
+def delete_list(list_name):
+    pass
+
+def edit_name(list_name, new_name):
+    pass
+
+def edit_id(list_name, new_id):
+    pass
