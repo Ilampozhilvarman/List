8cee00599858be49f9984e4c0e293549f2cd6922
Ilampozhilvarman <Ilampozhilvarman@gmail.com>
2026-07-09 11:43:33 -0500

    Final commands added to admin.py including help which lists all commands.

--- admin.py
+++ admin.py
@@ -1,7 +1,23 @@
+import subprocess
 import os
 import tools
+clear_command = command = "cls" if os.name == "nt" else "clear"
 command_line = input("> ")
 args = command_line.split()
+args_and_their_uses = {
+    "new": "Create a new list",
+    "push": "Add an item to a list",
+    "delete": "Remove an item from a list",
+    "update": "Update a list",
+    "get": "Get a list",
+    "del": "Delete a list",
+    "rtl": "Get a list",
+    "aci": "Get a list item",
+    "clr": "Clear a list",
+    "edit": "Edit an item in a list",
+    "clear": "Clear the terminal",
+}
+
 command = args[0]
 if len(args) == 3:
     if command == "new":
@@ -14,11 +30,11 @@ if len(args) == 3:
         tools.update_list(args[1], args[2])
 
 elif len(args) == 2:
-    if command == "get":
+    if command == "rtl":
         tools.get_list(args[1])
     elif command == "del":
         tools.delete_list(args[1])
-    elif command == "get":
+    elif command == "aci":
         tools.get_list_items(args[1])
     elif command == "clr":
         tools.clear_list(args[1])
@@ -29,4 +45,7 @@ elif len(args) == 3:
 
 elif len(args) == 1:
     if command == "clear":
-        os.system("clear")
+        subprocess.run([clear_command])
+    elif command == "help":
+        for command, use in args_and_their_uses.items():
+            print(f"{command}: {use}")
